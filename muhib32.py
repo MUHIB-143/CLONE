311f6d86e10ceb271936d04e81f98f65c764f042
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-15 01:26:21 -0800

    Update muhib32.py

--- muhib32.py
+++ muhib32.py
@@ -195,7 +195,7 @@ def xxr():
         tl = str(len(user))
         jalan('\033[0;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗')
         jalan(f'{xr}║{x}\x1b[0;92m║YOUR TOTAL IDS: {xr}'+tl)   
-        jalan(f'{x}{xr}║{x}\033[0;92m ║PLEASE WAIT YOUR CLONING PRO            ║') 
+        jalan(f'{x}{xr}║{x}\033[0;92m ║PLEASE WAIT YOUR CLONING PRO             ║') 
         jalan(f'\033[0;97m{xr}║{x}\033[0;93m ║USE YOUR MOBILE DATA ║')
         jalan(f'\033[0;97m{xr}║{x} \x1b[38;5;208m║Use Flight Mode For Speed Up║')
         jalan(f'\033[0;97m{xr}║{x} \033[0;95m║Super Fast Speed Cloning║')
