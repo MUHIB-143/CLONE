a3f76a6d2ddf7859304b67b61cbdbdadc5c6d7a6
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-16 01:54:12 +0600

    Update CLONE-M.py

--- CLONE-M.py
+++ CLONE-M.py
@@ -13,12 +13,12 @@ from time import sleep as waktu
 bitt=platform.architecture()[0]
 
 if bitt=="32bit":
-    os.system('clear');print('[!] YOUR DEVICE IS 32 BIT');time.sleep(1);print('\n\n[!] YOUR PYTHONVERSION :');time.sleep(1);os.system('python --version')
+    os.system('clear');print('[!] YOUR DEVICE IS 32 BIT');time.sleep(1);print('\n\n[!] YOUR PYTHON VERSION :');time.sleep(1);os.system('python --version')
     time.sleep(1)
    
 
 if bitt=="64bit":
-    os.system('clear');print('[!] YOUR DEVICE IS 64 BIT');time.sleep(1);print('\n\n[!] YOURPYTHONVERSION :');time.sleep(1);os.system('python --version')
+    os.system('clear');print('[!] YOUR DEVICE IS 64 BIT');time.sleep(1);print('\n\n[!] YOURPYTHON VERSION :');time.sleep(1);os.system('python --version')
     time.sleep(1)
 
 try:
