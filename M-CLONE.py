fe34f63497572d4bd842ca1fc7d0a4506518931e
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-16 01:39:58 +0600

    Update M-CLONE.py

--- M-CLONE.py
+++ M-CLONE.py
@@ -7,11 +7,11 @@ bitt=platform.architecture()[0]
 if bitt=="32bit":
     os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import MUHIB.muhib
+    import MUHIB
 
 if bitt=="64bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import MUHIB.muhib
+    import MUHIB
 
 
