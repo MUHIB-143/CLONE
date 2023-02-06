323ff36533a72938871aec39d7b0e2f10c121a03
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-02-06 15:58:31 +0600

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,4 +1,46 @@
-import socket
-hostname = socket.gethostname()
-myip = socket.gethostbyname(hostname)
-print("My ip adress is " + myip)
+Request URL: https://mbasic.facebook.com/login.php?next=https%3A%2F%2Fmbasic.facebook.com%2Fsecurity%2Fphotos%2Fa.10150266590131886%2F10154333075936886%2F%3Ftype%3D3%26p%3D30%26paipv%3D0&ref=104&rs=28&rid=31987371885&refsrc=deprecated
+Request Method: GET
+Status Code: 200 
+Remote Address: 31.13.64.18:443
+Referrer Policy: origin-when-cross-origin
+alt-svc: h3=":443"; ma=86400
+cache-control: private, no-cache, no-store, must-revalidate
+content-encoding: br
+content-security-policy: default-src data: blob: 'self' https://*.fbsbx.com 'unsafe-inline' *.facebook.com *.fbcdn.net 'unsafe-eval';script-src *.facebook.com *.fbcdn.net *.facebook.net *.google-analytics.com *.google.com 127.0.0.1:* 'unsafe-inline' blob: data: 'self' connect.facebook.net 'unsafe-eval';style-src fonts.googleapis.com *.fbcdn.net data: *.facebook.com 'unsafe-inline';connect-src *.facebook.com facebook.com *.fbcdn.net *.facebook.net wss://*.facebook.com:* wss://*.whatsapp.com:* wss://*.fbcdn.net attachment.fbsbx.com ws://localhost:* blob: *.cdninstagram.com 'self' http://localhost:3103 wss://gateway.facebook.com wss://edge-chat.facebook.com wss://snaptu-d.facebook.com wss://kaios-d.facebook.com/ v.whatsapp.net *.fbsbx.com *.fb.com;font-src data: *.gstatic.com *.facebook.com *.fbcdn.net *.fbsbx.com;img-src *.fbcdn.net *.facebook.com data: https://*.fbsbx.com *.tenor.co media.tenor.com facebook.com *.cdninstagram.com fbsbx.com fbcdn.net *.giphy.com connect.facebook.net *.carriersignal.info blob: android-webview-video-poster: googleads.g.doubleclick.net www.googleadservices.com *.whatsapp.net *.fb.com *.oculuscdn.com;media-src *.cdninstagram.com blob: *.fbcdn.net *.fbsbx.com www.facebook.com *.facebook.com https://*.giphy.com data:;frame-src *.doubleclick.net *.google.com *.facebook.com www.googleadservices.com *.fbsbx.com fbsbx.com data: www.instagram.com *.fbcdn.net https://paywithmybank.com https://sandbox.paywithmybank.com;worker-src blob: *.facebook.com data:;
+content-type: text/html; charset=utf-8
+cross-origin-opener-policy: unsafe-none
+date: Mon, 06 Feb 2023 09:46:37 GMT
+document-policy: force-load-at-top
+expires: Sat, 01 Jan 2000 00:00:00 GMT
+nel: {"report_to":"network-errors","max_age":3600,"failure_fraction":0.01}
+origin-agent-cluster: ?0
+pragma: no-cache
+priority: u=3,i
+report-to: {"max_age":259200,"endpoints":[{"url":"https:\/\/mbasic.facebook.com\/ajax\/mtouch_error_reports\/?device_level=unknown"}]}, {"max_age":3600,"endpoints":[{"url":"https:\/\/mbasic.facebook.com\/ajax\/mtouch_error_reports\/?device_level=unknown"}],"group":"network-errors"}
+set-cookie: datr=PkksYyXcUshL2m4bir7ov34I; expires=Wed, 05-Feb-2025 09:46:37 GMT; Max-Age=63072000; path=/; domain=.facebook.com; secure; httponly; SameSite=None
+strict-transport-security: max-age=15552000; preload
+vary: Accept-Encoding
+x-content-type-options: nosniff
+x-fb-debug: 7F9vv23oNCVl00/dDZpJHw0fIywFXQUKA3KxDawWSxqfL/jjqjdbUk85tGRP7Ld8zcGecIc6eJpPbtlxlPpgHw==
+x-fb-rlafr: 0
+x-frame-options: DENY
+x-xss-protection: 0
+:authority: mbasic.facebook.com
+:method: GET
+:path: /login.php?next=https%3A%2F%2Fmbasic.facebook.com%2Fsecurity%2Fphotos%2Fa.10150266590131886%2F10154333075936886%2F%3Ftype%3D3%26p%3D30%26paipv%3D0&ref=104&rs=28&rid=31987371885&refsrc=deprecated
+:scheme: https
+accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+accept-encoding: gzip, deflate, br
+accept-language: en-US,en;q=0.9
+cache-control: max-age=0
+cookie: sb=PEksY44ewwsK8iBuR4PvvXUz; datr=PkksYyXcUshL2m4bir7ov34I; dpr=0.75; locale=en_US; fr=01Rjr0BoZGGcYLh0O.AWUCjUyRWpHYR8Sa4CWgc2lqrps.BjcxsY.5d.AAA.0.0.Bj34ll.AWW1PmXBYQg; wd=1866x1209
+referer: https://mbasic.facebook.com/security/photos/a.10150266590131886/10154333075936886/?type=3&p=30&paipv=0
+sec-ch-ua: "Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"
+sec-fetch-dest: document
+sec-fetch-mode: navigate
+sec-fetch-site: same-origin
+sec-fetch-user: ?1
+upgrade-insecure-requests: 1
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36
